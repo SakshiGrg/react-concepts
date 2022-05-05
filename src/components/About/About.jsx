c8833a43cbcf75daddacbbac45febd82debfb76f
SakshiGrg <sakshigarg92@gmail.com>
2022-05-05 13:52:07 +0100

    usememo needed re-rendering happens on counter btn click

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,17 +2,36 @@ import React, {useState} from 'react';
 import Child from './Child';
 
 const About = (props) => {
+    const [number, setNumber] = useState(0);
+    const squaredNum = squareNum(number);
     const [count, setCount] = useState(0);
     console.log('Parent rendered');
+    const onChangeHandler = (e) => {
+        setNumber(e.target.value);
+    };
     return (
         <>
             <h1>{`${props.link} Component`}</h1>
-            <button onClick={() => setCount((c) => c + 1)}>Increase</button>
-            <p>Count: {count}</p>
+            <button onClick={() => setCount((c) => c + 1)}>counter++</button>
+            <p>Counter: {count}</p>
+            <br />
+            <input
+                type="number"
+                placeholder="Enter a number"
+                value={number}
+                onChange={onChangeHandler}
+            ></input>
+            <div>OUTPUT: {squaredNum}</div>
             {/* child component */}
             <Child name="ABCD" />
         </>
     );
+};
+
+// function to square the value
+const squareNum = (number) => {
+    console.log('Squaring will be done!-->', Math.pow(number, 2));
+    return Math.pow(number, 2);
 };
 
 export default About;
